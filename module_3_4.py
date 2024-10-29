7215c5018f17b59bad3e59a9be74c83d37201376
MaiklKluni <cool.misha13579@ya.ru>
2024-10-30 02:31:15 +0300

    HW 3_4

--- module_3_4.py
+++ module_3_4.py
@@ -7,4 +7,5 @@ def single_root_words(root_word, *other_words):
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
+
 print(result2)
